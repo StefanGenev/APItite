0ced1d2abe7f4cdfcb922abf2ef5494485275056
StefanGenev <stefangenev@abv.bg>
2024-01-26 17:03:43 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/APItite/ApItiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.APItite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApItiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
